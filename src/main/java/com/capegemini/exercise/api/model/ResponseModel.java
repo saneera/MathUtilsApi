fa2770bbc0be386581df23e2b4e38439a73bd8ec
Saneera Yapa <saneera@gmail.com>
2019-07-10 17:09:56 +0930

    add readme file

--- src/main/java/com/capegemini/exercise/api/model/ResponseModel.java
+++ src/main/java/com/capegemini/exercise/api/model/ResponseModel.java
@@ -1,6 +1,5 @@
 package com.capegemini.exercise.api.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
